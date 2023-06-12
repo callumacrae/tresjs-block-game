e3c3b257a53d5a9f682db0e0a530dd24799f98fc
Callum Macrae <callum@macr.ae>
2023-06-12 23:51:01 +0100

    use instancedmesh for terrain

--- src/modules/terrain/components/TerrainView.vue
+++ src/modules/terrain/components/TerrainView.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
+import { computed, toRefs } from "vue";
+import * as THREE from "three";
 import { useBlockData } from "../composables/blocks";
 
-const { data } = useBlockData();
+const { data } = toRefs(useBlockData());
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshStandardMaterial({ color: "lightgreen" });
+
+const mesh = computed(() => {
+  const mesh = new THREE.InstancedMesh(geometry, material, data.value.length);
+
+  const dummy = new THREE.Object3D();
+
+  for (let i = 0; i < data.value.length; i++) {
+    const block = data.value[i];
+    dummy.position.set(...block.position);
+    dummy.updateMatrix();
+    mesh.setMatrixAt(i, dummy.matrix);
+  }
+  return mesh;
+});
 </script>
 
 <template>
-  <TresMesh
-    v-for="block in data"
-    :key="block.position.join('-')"
-    :position="block.position"
-  >
-    <TresBoxGeometry :args="[1, 1, 1]" />
-    <TresMeshStandardMaterial color="lightgreen" />
-  </TresMesh>
+  <primitive :object="mesh" />
 </template>
